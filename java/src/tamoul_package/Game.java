fa8f3a1e863ed96d0b88e3f3f52cff3bf166a0c3
chriscml <100680847+chriscml@users.noreply.github.com>
2022-03-09 22:15:23 +0100

    test

---
+++ java/src/tamoul_package/Game.java
@@ -0,0 +1,25 @@
+package tamoul_package;
+
+//testttttouneeeeeejdizjiz
+
+public class Game {
+
+    public static void main(String[] args) {
+	Deck paquet = new Deck();
+	Player player1 = new Player("chris");
+	/*
+	 * for (int i = 0; i < 52; i++) {
+	 * System.out.println(paquet.getDeck().get(i).getRank() + " " +
+	 * paquet.getDeck().get(i).getSuit()); }
+	 */
+
+	/*
+	 * System.out.println("MELANGE --"); paquet.shuffle(); for (int i = 0; i < 52;
+	 * i++) { System.out.println(paquet.getDeck().get(i).getRank() + " " +
+	 * paquet.getDeck().get(i).getSuit()); }
+	 */
+
+	paquet.distrib(player1);
+    }
+
+}
