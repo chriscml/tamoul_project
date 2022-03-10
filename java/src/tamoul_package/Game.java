c6b0a6c1293f82aae60f3b2689e4e71645487446
chriscml <100680847+chriscml@users.noreply.github.com>
2022-03-10 22:47:55 +0100

    menu

--- java/src/tamoul_package/Game.java
+++ java/src/tamoul_package/Game.java
@@ -4,22 +4,30 @@ package tamoul_package;
 
 public class Game {
 
-    public static void main(String[] args) {
-	Deck paquet = new Deck();
-	Player player1 = new Player("chris");
-	/*
-	 * for (int i = 0; i < 52; i++) {
-	 * System.out.println(paquet.getDeck().get(i).getRank() + " " +
-	 * paquet.getDeck().get(i).getSuit()); }
-	 */
+	public static void main(String[] args) {
+		Deck paquet = new Deck();
+		Player player1 = new Player("chris");
+		/*
+		 * for (int i = 0; i < 52; i++) {
+		 * System.out.println(paquet.getDeck().get(i).getRank() + " " +
+		 * paquet.getDeck().get(i).getSuit()); }
+		 */
 
-	/*
-	 * System.out.println("MELANGE --"); paquet.shuffle(); for (int i = 0; i < 52;
-	 * i++) { System.out.println(paquet.getDeck().get(i).getRank() + " " +
-	 * paquet.getDeck().get(i).getSuit()); }
-	 */
+		/*
+		 * System.out.println("MELANGE --"); paquet.shuffle(); for (int i = 0; i < 52;
+		 * i++) { System.out.println(paquet.getDeck().get(i).getRank() + " " +
+		 * paquet.getDeck().get(i).getSuit()); }
+		 */
 
-	paquet.distrib(player1);
-    }
+		paquet.distrib(player1);
+		Menu menu = new Menu();
+		// menu.choixMenu();
+
+		System.out.print("Enter a character: ");
+		// Read the char
+		char ch = (char) System.in.read();
+		System.out.print("\n You pressed: " + ch);
+
+	}
 
 }
---
+++ java/src/tamoul_package/Menu.java
@@ -0,0 +1,21 @@
+package tamoul_package;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	// final int K_LEFT =
+
+	public Menu() {
+	}
+
+	public void choixMenu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choisir le menu < >");
+		char ch = (char) System.in.read();
+		System.out.println("tu as appuye sur" + ch);
+		/*
+		 * switch(input) { case: break; case: break; }
+		 */
+	}
+}
