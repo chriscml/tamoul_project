b504745c82e3008baa80036fff419d4c0839102b
chriscml <chriscamel69@gmail.com>
2022-03-11 02:27:48 +0100

    avancement, af:distrib

--- java/src/tamoul_package/Deck.java
+++ java/src/tamoul_package/Deck.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 
 public class Deck {
 	ArrayList<Card> deck = new ArrayList<>();
+	ArrayList<Card> discard = new ArrayList<>();
+	int nombre_aleatoire = 0, nombre_aleatoire_anc = 0, max = 52, min = 0;
 
 	public Deck() {
 		for (SuitEnum suit : SuitEnum.values()) {
@@ -22,20 +24,44 @@ public class Deck {
 		return deck;
 	}
 
-	public void distrib(Player player) {
-		int nombre_aleatoire = 0, nombre_aleatoire_anc = 0, max = 51;
+	public ArrayList<Card> getDiscard() {
+		return discard;
+	}
+
+	public void distrib(Menu menu) {
+		/*
+		 * for (int i = 0; i < 4; i++) { do { nombre_aleatoire = min + (int)
+		 * (Math.random() * ((max - min) + 1)); } while (nombre_aleatoire ==
+		 * nombre_aleatoire_anc); nombre_aleatoire_anc = nombre_aleatoire; max--;
+		 * player.deck_player.add(new Card(getDeck().get(nombre_aleatoire).getRank(),
+		 * getDeck().get(nombre_aleatoire).getSuit())); deck.remove(nombre_aleatoire);
+		 * System.out.println(player.deck_player.get(i).getRank() + " " +
+		 * player.deck_player.get(i).getSuit()); } System.out.println(max + "\n");
+		 */
+
+		int cpt = 0;
+		// shuffle();
 		for (int i = 0; i < 4; i++) {
-			do {
-				nombre_aleatoire = 0 + (int) (Math.random() * ((max - 0) + 1));
-			} while (nombre_aleatoire == nombre_aleatoire_anc);
-			nombre_aleatoire_anc = nombre_aleatoire;
-			max--;
-			player.deck_player.add(
-					new Card(getDeck().get(nombre_aleatoire).getRank(), getDeck().get(nombre_aleatoire).getSuit()));
-			deck.remove(nombre_aleatoire);
-			System.out.println(player.deck_player.get(i).getRank() + " " + player.deck_player.get(i).getSuit());
+			for (int j = 0; j < menu.getNbOfPlayers(); j++) {
+				menu.players.get(j).deck_player.add(deck.get(51 - cpt));
+				// deck.remove(cpt);
+				System.out.println(
+						"player" + (j + 1) + " carte" + cpt + " : " + menu.players.get(j).deck_player.get(i).getRank()
+								+ " " + menu.players.get(j).deck_player.get(i).getSuit());
+				cpt = cpt + 1;
+			}
+			System.out.println("\n ------------------------ \n");
 		}
 
+		/*
+		 * for (int i = 0; i < (4 * menu.getNbOfPlayers()); i++) { deck.remove(i); }
+		 */
+	}
+
+	public void drawCard(Deck paquet) {
+		int i = 0;
+		paquet.getDiscard().add(paquet.getDeck().get(getDeck().size() - 1));
+		System.out.println(paquet.getDiscard().get(i).getRank() + " " + paquet.getDiscard().get(i).getSuit());
 	}
 
 }
--- java/src/tamoul_package/Game.java
+++ java/src/tamoul_package/Game.java
@@ -6,12 +6,6 @@ public class Game {
 
 	public static void main(String[] args) {
 		Deck paquet = new Deck();
-		Player player1 = new Player("chris");
-		/*
-		 * for (int i = 0; i < 52; i++) {
-		 * System.out.println(paquet.getDeck().get(i).getRank() + " " +
-		 * paquet.getDeck().get(i).getSuit()); }
-		 */
 
 		/*
 		 * System.out.println("MELANGE --"); paquet.shuffle(); for (int i = 0; i < 52;
@@ -19,15 +13,19 @@ public class Game {
 		 * paquet.getDeck().get(i).getSuit()); }
 		 */
 
-		paquet.distrib(player1);
 		Menu menu = new Menu();
-		// menu.choixMenu();
+		paquet.distrib(menu);
 
-		System.out.print("Enter a character: ");
-		// Read the char
-		char ch = (char) System.in.read();
-		System.out.print("\n You pressed: " + ch);
+		for (int i = 0; i < 52 - (4 * menu.getNbOfPlayers()); i++) {
+			System.out.println(paquet.getDeck().get(i).getRank() + " " + paquet.getDeck().get(i).getSuit());
+		}
+		System.out.println("\n-_-_-_-__-_-_-_-_-_-_-_-_-__-_-_-_-\n");
 
+		paquet.drawCard(paquet);
+		paquet.drawCard(paquet);
+		paquet.drawCard(paquet);
+		paquet.drawCard(paquet);
+		paquet.drawCard(paquet);
 	}
 
 }
